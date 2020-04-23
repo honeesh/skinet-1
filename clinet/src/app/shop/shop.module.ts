f94c83318d4ead300f598ad07ebbb06c6f15202d
Abedu ALRhman Honeesh <hhn1000@gmail.com>
2020-04-23 20:05:17 +0300

    End of section 10

--- clinet/src/app/shop/shop.module.ts
+++ clinet/src/app/shop/shop.module.ts
@@ -4,15 +4,17 @@ import { ShopComponent } from './shop.component';
 
 import { SharedModule } from '../shared/shared.module';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [ShopComponent]
+    SharedModule,
+    ShopRoutingModule
+  ]
 })
 export class ShopModule { }
